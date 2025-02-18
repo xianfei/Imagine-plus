d9e02260d3c6c756220d7868933da8929c679bba
xianfei <xianfei@bupt.cn>
2025-02-18 14:14:04 +0800

    Revert "Try to use custom-electron-titlebar"
    This reverts commit b787cdecf2a7d8d60312464348fe1153b59360d2.

--- modules/bridge/preload.ts
+++ modules/bridge/preload.ts
@@ -3,14 +3,6 @@ import logger from 'electron-log'
 import { IpcChannel } from '../common/types'
 import { createAsyncCall } from './async-call/renderer'
 import { ImagineAPI } from './interface'
-import { Titlebar, TitlebarColor } from "custom-electron-titlebar";
-
-window.addEventListener('DOMContentLoaded', () => {
-  // Title bar implementation
-  new Titlebar({
-    backgroundColor: TitlebarColor.fromHex('#FFFFFF')
-  });
-});
 
 const imagineAPI: ImagineAPI = {
   logger,
